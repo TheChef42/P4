a6cc6c38434b08bce6ca7afa7bad924d603c9725
TheChef42 <danielflute@gmail.com>
2023-02-24 12:03:17 +0100

    nu med oprettelser af brugere fra kode

---
+++ welcome/src/test.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class test {
+    private JButton button1;
+    private JProgressBar progressBar1;
+    private JPasswordField passwordField1;
+    private JTextField textField1;
+}
