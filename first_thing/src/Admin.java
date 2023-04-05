d59d0355c44522339a128c654224c2e70898ebc6
TheChef42 <danielflute@gmail.com>
2023-04-05 14:48:59 +0200

    user

--- first_thing/src/Admin.java
+++ first_thing/src/Admin.java
@@ -1,4 +1,6 @@
 import java.sql.*;
+import java.util.Scanner;
+
 public class Admin {
 
     //public Admin(){}
@@ -8,16 +10,41 @@ public class Admin {
     private void viewUsers(){
         try {
             Connection con = ConnectionManager.getConnection();
-            String qry = "SELECT * customers";
+            String qry = "SELECT * FROM customer";
             PreparedStatement st = con.prepareStatement(qry);
             ResultSet rs = st.executeQuery();
-            
+            System.out.print("id" + "\t\t");
+            System.out.print("email" + "\t\t");
+            System.out.print("password" + "\t\t");
+            System.out.print(("firstname") + "\t\t");
+            System.out.print(("lastname") + "\t\t");
+            System.out.print(("balance") + "\t\t");
+            System.out.println(("created_at"));
+            while(rs.next()){
+                System.out.print(rs.getInt("id") + "\t\t");
+                System.out.print(rs.getString("email") + "\t\t");
+                System.out.print(rs.getString("password") + "\t\t");
+                System.out.print(rs.getString("firstname") + "\t\t");
+                System.out.print(rs.getString("lastname") + "\t\t");
+                System.out.print(rs.getFloat("balance") + "\t\t");
+                System.out.println(rs.getTimestamp("created_at"));
+            }
+
         } catch(SQLException e){
             e.printStackTrace();
         }
     }
-    public void deleteUser(){
+    public void deleteUser(int id){
         //TODO: implement hpw to delete a user as admin
+        try{
+            Connection con = ConnectionManager.getConnection();
+            String qry = "DELETE FROM customer WHERE id = ?";
+            PreparedStatement st = con.prepareStatement(qry);
+            st.setInt(1,id);
+            st.executeUpdate();
+    }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
     public int viewProducts(){
         //TODO: implement how to view and select a product
@@ -38,12 +65,17 @@ public class Admin {
         }
     }
     public static void main(String[] args){
-        //Admin chris = new Admin();
-        //chris.createProduct("sprite",10,200);
+        Admin chris = new Admin();
         Products cola = new Products(2);
         System.out.println(cola.getName() + cola.getPrice());
         System.out.println("Du go");
-
+        chris.viewUsers();
+        System.out.print("Who do you want to delete, bitch!");
+        Scanner scan = new Scanner(System.in);
+        int in = scan.nextInt();
+        chris.deleteUser(in);
+        chris.viewUsers();
+        Products.getProducts();
     }
     public void updateProduct(){
         //TODO: implement updateProduct
--- first_thing/src/Transaction.java
+++ first_thing/src/Transaction.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Transaction {
     private int id;
     //private Time date;
@@ -6,6 +11,25 @@ public class Transaction {
 
     public String[] getProducts() {
         //TODO: implement how to return the products
+        try {
+            Connection con = ConnectionManager.getConnection();
+            String qry = "SELECT * FROM products";
+            PreparedStatement st = con.prepareStatement(qry);
+            ResultSet rs = st.executeQuery();
+            System.out.print("id" + "\t\t");
+            System.out.print("product" + "\t\t");
+            System.out.print("price" + "\t\t");
+            System.out.print(("stock") + "\t\t");
+            while(rs.next()){
+                System.out.print(rs.getInt("id") + "\t\t");
+                System.out.print(rs.getString("product") + "\t\t");
+                System.out.print(rs.getString("price") + "\t\t");
+                System.out.print(rs.getString("stock") + "\t\t");
+            }
+
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
         return products;
     }
     public void getTransactionsList(){
